d9e846e12cf16a03197f2631bb7cb309d8d1be0f
Henrique <henriquetfs52@gmail.com>
2026-04-22 21:15:03 -0300

    Lab4 finalizado

--- Lab4/guess.js
+++ Lab4/guess.js
@@ -1,2 +1,34 @@
-document.getElementById("numero").ariaValueMax;
-console.log(valor);
+let x = Math.floor(Math.random() * 100)
+
+function verificar(){
+    let y = document.getElementById("numero").value
+    if (x == y){
+        document.getElementById("resp").style.color = "green"
+        return y
+    }
+    if (x>y){
+        let z = "o número é maior que " + y
+        document.getElementById("resp").style.color = "red"
+        return z
+    }
+    if (x<y){
+        let z = "o numero é menor que " + y
+        document.getElementById("resp").style.color = "red"
+        return z
+    }
+}
+
+
+function print(){
+    let y = document.getElementById("numero").value
+    document.getElementById("resp").innerHTML = verificar()
+    if (x>y){
+        document.getElementById("menores").innerHTML += " " +y
+
+    }
+    if (x<y){
+        let z = "o numero é menor que " + y
+        document.getElementById("maiores").innerHTML += " " +y
+
+    }
+}
