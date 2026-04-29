d38256e7c6cd9bedcb581e649f921765f0fa26a2
Henrique Teixeira <henriquetfs52@gmail.com>
2026-04-29 03:02:44 -0300

    index do projeto jogo finalizado

---
+++ Projeto jogo/script.js
@@ -0,0 +1,25 @@
+let canvas = document.getElementById("Ctitulo")
+let ctx = canvas.getContext("2d");
+
+let img = new Image()
+    img.src = "Bullet-removebg-preview.png"
+x = -150
+vel = 6
+
+function desenhar_arma() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, x, 5, 150, 150);
+}
+
+function animar(){
+    x+=vel
+    
+    if (x == canvas.width + img.width){ 
+        x=-150
+    }
+    desenhar_arma()
+
+    requestAnimationFrame(animar)
+}
+
+animar()
