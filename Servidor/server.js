028ee33b7a40fdabebba3191dc83c06b635c1fc3
Henrique <hemriquetfs52@gmail.com>
2026-05-06 16:47:45 -0300

    .

--- Servidor/server.js
+++ Servidor/server.js
@@ -3,6 +3,17 @@ var http = require("http")
 var express = require("express")
 var bodyParser = require("body-parser");
 
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+const uri = `mongodb+srv://henriquetfs52:7SiAZI0vMuObM80u@projeto1.cop1elj.mongodb.net/?appName=projeto1`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true })
+
+var dbo = client.db("exemplo_bd");
+var usuarios = dbo.collection("usuarios");
+
 
 var app = express();
 app.use(express.static("./public"))
@@ -14,7 +25,7 @@ app.set('views', './views');
 var server = http.createServer(app);
 server.listen(80)
 
-console.log("Servidor rodando 67".rainbow)
+console.log("Servidor rodando".rainbow)
 
 app.get("/", function(req, res){
     res.redirect("Aula")
@@ -51,4 +62,42 @@ app.post("/cadastro", function(req,res){
 app.get("/for", function(req,res){
     var qtde = req.query.qtde
     res.render("exemplo_for.ejs",{qtde})
+})
+
+app.post("/cadastrar_usuario", function(req,res){
+var data = {db_nome: req.body.nome,
+            db_login: req.body.login,
+            db_senha: req.body.senha
+}
+
+usuarios.insertOne(data,function (err) {
+      console.log(err)
+    if (err) {
+        res.render('resposta_usuario', {resposta: "Erro ao cadastrar usuário!"})
+      }else {
+        res.render('resposta_usuario', {resposta: "Usuário cadastrado com sucesso!"})        
+      };
+    });
+
+
+})
+
+app.post("/logar_usuario", function(req,res){
+var data = {
+            db_login: req.body.login,
+            db_senha: req.body.senha
+        }
+
+usuarios.find(data).toArray(function(err, items){
+    console.log(items)
+    if (items.length == 0)  {
+        res.render('resposta_usuario', {resposta: "Usuário/senha não encontrado!"})
+    }
+    else if (err) {
+        res.render('resposta_usuario', {resposta: "Erro ao logar usuário!"})
+      }
+      else {
+        res.render('resposta_usuario', {resposta: "Usuário logado com sucesso!"})        
+      };
+    });
 })
