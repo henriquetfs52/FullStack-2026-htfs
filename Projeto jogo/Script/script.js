915163e620f906193d2beabb71be098d632771f7
Henrique Teixeira <henriquetfs52@gmail.com>
2026-05-20 00:34:39 -0300

    .

--- Projeto jogo/Script/script.js
+++ Projeto jogo/Script/script.js
@@ -1,16 +1,19 @@
 let canvas = document.getElementById("Ctitulo")
 let ctx = canvas.getContext("2d");
 
+// Imagem da bala do menu
 let img = new Image()
     img.src = "../Sprites/Bullet-removebg-preview.png"
 x = -150
 vel = 6
 
+// Função que desenha a img
 function desenhar_arma() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(img, x, 5, 150, 150);
 }
 
+// Função que anima a img
 function animar(){
     x+=vel
     
